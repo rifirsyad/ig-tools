0db79178933c40d4be27f3d5b35049c77155ba02
Rifqi Irsyadillah ✪ <rifirsyad@gmail.com>
2019-04-30 16:02:27 +0700

    Update botlike.js

--- botlike.js
+++ botlike.js
@@ -1,5 +1,9 @@
 'use strict'
 
+// Recode by officialputuid
+// Last modified by I Putu Jaya Adi Pranata (officialputuid) on March 30, 2019
+// fb|ig|twitter|gplus|line|github|behance|medium? officialputuid & https://officialputu.id
+
 const Client = require('instagram-private-api').V1;
 const delay = require('delay');
 const chalk = require('chalk');
@@ -7,35 +11,35 @@ const _ = require('lodash');
 const inquirer = require('inquirer');
 
 const User = [
-    {
-        type:'input',
-        name:'username',
-        message:'[>] Insert Username:',
-	validate: function(value){
-		if(!value) return 'Can\'t Empty';
-		return true;
-	}
-    },
-    {
-        type:'password',
-        name:'password',
-        message:'[>] Insert Password:',
-        mask:'*',
-	validate: function(value){
-		if(!value) return 'Can\'t Empty';
-		return true;
-	}
-    },
-    {
-        type:'input',
-        name:'sleep',
-        message:'[>] Insert Sleep (MiliSeconds):',
-        validate: function(value){
-            value = value.match(/[0-9]/);
-            if (value) return true;
-            return 'Delay is number';
-        }
+{
+    type:'input',
+    name:'username',
+    message:'Insert Username:',
+    validate: function(value){
+      if(!value) return 'Can\'t Empty';
+      return true;
+  }
+},
+{
+    type:'password',
+    name:'password',
+    message:'Insert Password:',
+    mask:'*',
+    validate: function(value){
+      if(!value) return 'Can\'t Empty';
+      return true;
+  }
+},
+{
+    type:'input',
+    name:'sleep',
+    message:'Insert Sleep (In MiliSeconds):',
+    validate: function(value){
+        value = value.match(/[0-9]/);
+        if (value) return true;
+        return 'Delay is number';
     }
+}
 ]
 
 const Login = async function(User){
@@ -58,32 +62,36 @@ const Login = async function(User){
 const Like = async function(session,media){
     try {
         if (media.params.hasLiked) {
-           return chalk`{bold.blue Already Liked}`;
-        }
-        await Client.Like.create(session, media.id);
-        return chalk`{bold.green Success Like}`;
-    } catch (err) {
-        return chalk`{bold.red Failed Like}`;
-    }
+         return chalk`{bold.blue Already Liked}`;
+     }
+     await Client.Like.create(session, media.id);
+     return chalk`{bold.green Success Liked}`;
+ } catch (err) {
+    return chalk`{bold.red Failed Liked}`;
+}
 }
 
 const Excute = async function(User, sleep){
     try {
-        console.log(chalk`\n{yellow [?] Try to Login . . .}`);
-        const doLogin = await Login(User);
-        console.log(chalk`{green [!] Login Succsess}, {yellow [?] Try Like All Media in Feed / Timeline . . .\n}`);
+		console.log(chalk`{yellow \n? Try to Login . . .}`)
+		const doLogin = await Login(User);
+		console.log(chalk`{green ✓ Login Succsess. }{yellow ? Try Get All Media in Feed / Timeline . . .}`)
         const feed = new Client.Feed.Timeline(doLogin.session);
+		console.log(chalk`{green ✓ Succsess To Get List All Media in Feed / Timeline » ${User.username}\n}`);
         var cursor;
+		console.log(chalk`{yellow ≡ READY TO START BOTLIKE V1 WITH RATIO RAND 1-5 TARGET/${sleep} MiliSeconds\n}`)
         do {
+            var timeNow = new Date();
+            timeNow = `${timeNow.getHours()}:${timeNow.getMinutes()}:${timeNow.getSeconds()}`
             if (cursor) feed.setCursor(cursor);
             var media = await feed.get();
-            media = _.chunk(media, 10);
+            media = _.chunk(media, 5);
             for (var i = 0; i < media.length; i++) {
                 await Promise.all(media[i].map(async (media) => {
                     const doLike = await Like(doLogin.session, media);
-                    console.log(chalk`[{bold.green Username:}] ${media.params.user.username}\n[{cyan ${media.id}}] => [${doLike}]`);
+                    console.log(chalk`{magenta ⌭ ${timeNow}}: ${media.params.user.username} ➾ {cyan ${media.id}} ➾ ${doLike}`);
                 }))
-                await console.log(chalk`{yellow \n [#][>] Delay For ${sleep} MiliSeconds [<][#] \n}`);
+                await console.log(chalk`{yellow \nϟ Current Account: {bold.green ${User.username}} » Delay: RAND 1-5/${sleep}ms\n}`);
                 await delay(sleep);
             }
         } while(feed.isMoreAvailable());
@@ -92,24 +100,16 @@ const Excute = async function(User, sleep){
     }
 }
 
-console.log(chalk`
- {bold.cyan
-  —————————————————— [INFORMATION] ————————————————————
-
-  [?] {bold.green BOTLIKEv1 | Like/Love TL IG *SET SLEEP!}
-
-  ——————————————————  [THANKS TO]  ————————————————————
-  [✓] CODE BY CYBER SCREAMER CCOCOT (ccocot@bc0de.net)
-  [✓] FIXING & TESTING BY SYNTAX (@officialputu_id)
-  [✓] CCOCOT.CO | BC0DE.NET | NAONLAH.NET | WingkoColi
-  [✓] SGB TEAM REBORN | Zerobyte.id | ccocot@bc0de.net 
-  —————————————————————————————————————————————————————}
-      `);
+console.log(chalk`{bold.cyan
+  Ξ TITLE  : BOTLIKE TIMELINE v1 [Set Sleep]
+  Ξ CODE   : CYBER SCREAMER CCOCOT (ccocot@bc0de.net)
+  Ξ STATUS : {bold.red [-ITTWY]} & {bold.yellow [TESTED]}}
+  `);
 
 inquirer.prompt(User)
-    .then(answers => {
-        Excute({
-            username:answers.username,
-            password:answers.password
-        },answers.sleep);
-    })
+.then(answers => {
+    Excute({
+        username:answers.username,
+        password:answers.password
+    },answers.sleep);
+})
